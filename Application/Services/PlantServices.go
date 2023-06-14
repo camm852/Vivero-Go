223da6859cd9698ea83b28cafc46aba58d416752
camm852 <camm852@gmail.com>
2023-06-13 22:15:10 -0500

    Asignaciones

--- Application/Services/PlantServices.go
+++ Application/Services/PlantServices.go
@@ -2,24 +2,23 @@ package Services
 
 import (
 	"errors"
-
 	Entities "proyecto.com/Domain/Entities"
 )
 
-func GetPlants() []Entities.Plant {
+func GetPlants() []Entities.Plant { //Carlos
 	var _plants Entities.IPlantRepository = Entities.Plant{}
 	var plants []Entities.Plant
 	plants = _plants.GetPlants()
 	return plants
 }
 
-func GetPlant(id uint) (Entities.Plant, error) {
+func GetPlant(id uint) (Entities.Plant, error) { //Danilo
 	var _plant Entities.IPlantRepository = Entities.Plant{}
 	plant, err := _plant.GetPlant(id)
 	return plant, err
 }
 
-func NewPlant(plant *Entities.Plant) bool {
+func NewPlant(plant *Entities.Plant) bool { //Hugo
 	var _plant Entities.IPlantRepository = Entities.Plant{}
 
 	var plantCreated = _plant.NewPlant(plant)
@@ -27,32 +26,14 @@ func NewPlant(plant *Entities.Plant) bool {
 	return plantCreated
 }
 
-func UpdatePlant(plant *Entities.Plant) bool {
+func UpdatePlant(plant *Entities.Plant) bool { //Martin
 	var _plant Entities.IPlantRepository = Entities.Plant{}
 	isUpdated := _plant.UpdatePlant(plant)
 
 	return isUpdated
 }
 
-func AddNutrient(plant Entities.Plant, amountNutrient uint) error {
-	if plant.AmountNutrientsRequired == 0 {
-		return errors.New("Division by zero")
-	}
-	plant.AmountNutrientsSystem += float64(amountNutrient) / float64(plant.AmountNutrientsRequired)
-	plant.UpdatePlant(&plant)
-	return nil
-}
-
-func AddWater(plant Entities.Plant, amountWater float64) error {
-	if plant.AmountWaterRequired == 0 {
-		return errors.New("Division by zero")
-	}
-	plant.AmountWaterSystem += float64(amountWater) / float64(plant.AmountWaterRequired)
-	plant.UpdatePlant(&plant)
-	return nil
-}
-
-func DeletePlant(id uint) bool {
+func DeletePlant(id uint) bool { //Danilo
 	var _plant Entities.IPlantRepository = Entities.Plant{}
 	Plant2Delete, err := _plant.GetPlant(id)
 
@@ -64,7 +45,25 @@ func DeletePlant(id uint) bool {
 	}
 }
 
-func AddManyNutrient(plants []Entities.Plant, amountNutrient uint) error {
+func AddNutrient(plant Entities.Plant, amountNutrient uint) error { //Abella
+	if plant.AmountNutrientsRequired == 0 {
+		return errors.New("Division by zero")
+	}
+	plant.AmountNutrientsSystem += float64(amountNutrient) / float64(plant.AmountNutrientsRequired)
+	plant.UpdatePlant(&plant)
+	return nil
+}
+
+func AddWater(plant Entities.Plant, amountWater float64) error { //Martin
+	if plant.AmountWaterRequired == 0 {
+		return errors.New("Division by zero")
+	}
+	plant.AmountWaterSystem += float64(amountWater) / float64(plant.AmountWaterRequired)
+	plant.UpdatePlant(&plant)
+	return nil
+}
+
+func AddManyNutrient(plants []Entities.Plant, amountNutrient uint) error { //Abella
 	var err error
 	for _, plant := range plants {
 		err = AddNutrient(plant, amountNutrient)
@@ -72,7 +71,7 @@ func AddManyNutrient(plants []Entities.Plant, amountNutrient uint) error {
 	return err
 }
 
-func AddManyWater(plants []Entities.Plant, amountWater float64) error {
+func AddManyWater(plants []Entities.Plant, amountWater float64) error { //Hugo
 
 	var err error
 	for _, plant := range plants {
@@ -82,7 +81,7 @@ func AddManyWater(plants []Entities.Plant, amountWater float64) error {
 
 }
 
-func DecreaseWater() {
+func DecreaseWater() { // Martin
 	plants := GetPlants()
 
 	for i := range plants {
@@ -113,7 +112,7 @@ func DecreaseWater() {
 	}
 }
 
-func DecreaseNutrients() {
+func DecreaseNutrients() { //Hugo
 	plants := GetPlants()
 
 	for i := range plants {
@@ -122,14 +121,6 @@ func DecreaseNutrients() {
 		if plant.AmountNutrientsSystem == 0 {
 			//logica para que la planta muera
 			continue
-		} else if plant.AmountNutrientsSystem > (1 + plant.DegreeSurvival) {
-			excessNutrients := plant.AmountNutrientsSystem - (plant.AmountWaterRequired + plant.DegreeSurvival)
-			if excessNutrients > plant.DegreeSurvival {
-				//logica para obtener el tiempo en el que la planta muere por exceso de agua
-				plant.AmountNutrientsSystem -= 2 * (1 / float64(plant.DegreeHydration))
-			} else {
-				plant.AmountNutrientsSystem -= 2 * (1 / float64(plant.DegreeHydration))
-			}
 		} else if plant.AmountNutrientsSystem < plant.DegreeSurvival {
 			plant.AmountNutrientsSystem -= 2 * (1 / float64(plant.DegreeHydration))
 		} else {
